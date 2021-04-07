8b0a9ad650895f172a643c81590cfc8d7770f109
jeromejaglale <jerome.jaglale@gmail.com>
2021-04-07 21:16:10 +0000

    Add script to load users

---
+++ scripts/load_users.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+SCRIPT_DIR=`dirname "$0"`
+
+# Notes:
+# docker-compose --rm: delete container afterwards 
+# "ireceptor-gateway" is the Docker service name, as defined in docker-compose.yml 
+# sh -c '...' is the command executed inside the container
+sudo -E docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-gateway run --rm \
+			ireceptor-gateway \
+				sh -c 'php artisan db:seed --class=UserSeeder'
