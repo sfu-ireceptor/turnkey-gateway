c4e6f75051574e5e68e61e3a7dde9849a76991d9
jeromejaglale <jerome.jaglale@gmail.com>
2021-04-07 20:55:44 +0000

    Add cache refresh

--- scripts/load_private_repositories.sh
+++ scripts/load_private_repositories.sh
@@ -9,3 +9,12 @@ SCRIPT_DIR=`dirname "$0"`
 sudo -E docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-gateway run --rm \
 			ireceptor-gateway \
 				sh -c 'php artisan db:seed --class=RestServicePrivateSeeder'
+
+# refresh cached repertoires (for form widgets and charts)
+echo
+echo "Refreshing cached repertoires.."
+sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-gateway exec -T ireceptor-gateway \
+		sh -c 'php artisan sample:cache'
+echo
+echo "Done"
+echo
